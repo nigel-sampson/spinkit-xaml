5de80cead02b50876da9d4a213a837099a137137
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-01-13 09:02:37 +1300

    Solution / Project created

---
+++ Spinkit.Xaml/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Spinkit.Xaml
+{
+    public sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var frame = Window.Current.Content as Frame;
+
+            if (frame == null)
+            {
+                frame = new Frame
+                {
+                    Language = Windows.Globalization.ApplicationLanguages.Languages[0]
+                };
+
+                Window.Current.Content = frame;
+            }
+
+            if (frame.Content == null)
+            {
+                frame.Navigate(typeof (DemoView));
+            }
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ Spinkit.Xaml/DemoView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spinkit.Xaml
+{
+    public sealed partial class DemoView
+    {
+        public DemoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
