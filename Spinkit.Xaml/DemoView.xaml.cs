e0758f6c01b6a2895c15e7cd75d05828eb9adfc0
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-01-19 09:11:50 +1300

    Credits

--- Spinkit.Xaml/DemoView.xaml.cs
+++ Spinkit.Xaml/DemoView.xaml.cs
@@ -15,5 +15,15 @@ namespace Spinkit.Xaml
         {
             await Launcher.LaunchUriAsync(new Uri("https://github.com/nigel-sampson/spinkit-xaml"));
         }
+
+        private async void OnViewCreated(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://twitter.com/nigelsampson"));
+        }
+
+        private async void OnViewInspired(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://twitter.com/tobiasahlin"));
+        }
     }
 }
