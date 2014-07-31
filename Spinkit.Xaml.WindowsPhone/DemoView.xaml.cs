0a4f11b918e63a8a3a54059ad35a37e2ab0eaeeb
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-07-30 20:25:59 -0400

    Windows Phone 8.1 support

---
+++ Spinkit.Xaml.WindowsPhone/DemoView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.System;
+using Windows.UI.Xaml;
+
+namespace Spinkit.Xaml
+{
+    public sealed partial class DemoView
+    {
+        public DemoView()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnViewSource(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://github.com/nigel-sampson/spinkit-xaml"));
+        }
+
+        private async void OnViewCreated(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://twitter.com/nigelsampson"));
+        }
+
+        private async void OnViewInspired(object sender, RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri("https://twitter.com/tobiasahlin"));
+        }
+    }
+}
